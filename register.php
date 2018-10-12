88f28c679f932f4bfec867532f9fd616466f48bf
Faisal-Ahmad <43377742+Faisal-Ahmad@users.noreply.github.com>
2018-10-12 23:09:17 +0600

    Update register.php

--- register.php
+++ register.php
@@ -1,8 +1,11 @@
-<html>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 </head>
 <body>
-</body>
+
 <form name="application" method="post" action="login.php">
 <br/><br/><br/>
 <!--Table starts -->
@@ -116,5 +119,5 @@
 </table>
 
 </form>
-
+</body>
 </html>
